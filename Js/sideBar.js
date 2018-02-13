626fa4111dcb073606d9b836d8d896a9823d2db5
cjwakefield <colin.j.wakefield@gmail.com>
2018-02-13 15:55:37 -0600

    side bar

---
+++ Js/sideBar.js
@@ -0,0 +1,12 @@
+﻿/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("Sidenav").style.width = "250px";
+    //alert("open"); 
+
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("Sidenav").style.width = "0";
+    //alert("Closed"); 
+}
